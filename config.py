e35cd340fb4cefc6e30fee8cf80362dc26448258
dyno01 <113084283+dyno01@users.noreply.github.com>
2025-09-25 18:45:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,11 +20,11 @@ YTUB_COOKIES = """
 API_ID = os.getenv("API_ID", "")
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8215640667:AAHnA8ZDdHCFkqn_JbdsTSGGwEQ92Bg_uoE")
-MONGO_DB = os.getenv("MONGO_DB", "")
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://ishwarrathod24_db_user:CRmzfi4mkb5d1bKD@cluster0.iczbpnc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "2031933716").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001234456")) # optional with -100
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-4822980359")) # optional with -100
 FORCE_SUB = int(os.getenv("FORCE_SUB", "-10012345567")) # optional with -100
 MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
@@ -34,6 +34,7 @@ FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
+
 
 
 
